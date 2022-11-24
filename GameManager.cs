ce2f9a9d1d1bf0543f8423ae3a0c3d8baa7a8eaa
kiki0323 <118674857+kiki0323@users.noreply.github.com>
2022-11-25 02:18:01 +0800

    第四次修改GameManage

--- GameManager.cs
+++ GameManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;  // 使用 UI 物件，必須增加此處理程序
@@ -13,10 +14,10 @@ public class GameManager : MonoBehaviour
 
     void Start() // 將場景中的物件放到已宣告的各個物件中
     {
-        car = GameObject.Find("car");         
+        car = GameObject.Find("car");
         flag = GameObject.Find("flag");
         distance = GameObject.Find("Distance");
-        score=GameObject .Find("score");
+        score = GameObject.Find("score");
         y = GameObject.Find("y");
 
     }
@@ -27,36 +28,23 @@ public class GameManager : MonoBehaviour
         float length = flag.transform.position.x - car.transform.position.x;
         // 將旗子與汽車的距離顯示在UI上面
         distance.GetComponent<Text>().text = "距離目標還有 " + length.ToString("F2") + "m";
-        // 計算空物件與汽車的距離
-        float eu = car.transform.position.x - y.transform.position.x+ flag.transform.position.x;
-        float i = 0;
-
-        if (eu >= 14.9f || eu < 0)
+        if (Input.GetMouseButtonUp(0))
         {
-            i = 100 / length;
-            if (i > 100)
-                i = 100;
-
+            if (length > 0)
+            {
+                // 計算空物件與汽車的距離
+                float eu = (car.transform.position.x - y.transform.position.x) / (flag.transform.position.x - y.transform.position.x);
+                float i = eu * 100;
+                score.GetComponent<Text>().text = "分數 " + i.ToString("F2") + "分";
+            }
+            else if (length < 0)
+            {
+                score.GetComponent<Text>().text = "分數: 0分";
+            }
         }
-        else
-        {
-            i = 0;
-        }
-        float x = car.transform.position.x - y.transform.position.x + flag.transform.position.x;
-        float u = 0;
-
-        if (eu >= 6.90f || eu < 0)
-        {
-            x = 100 / length;
-            if (u > 100)
-                u = 100;
-
-        }
-        else
-        {
-            u = 0;
-        }
-        score.GetComponent<Text>().text = "分數 " + u.ToString("F2") + "分";
     }
 }
+    
+   
+            
 
